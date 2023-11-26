6e836b3543b8daf939c016d968f5add1f3785484
Knarb <knarbmakes@gmail.com>
2023-11-25 22:24:16 -0500

    Try different base.

--- run_in_docker_with_git.sh
+++ run_in_docker_with_git.sh
@@ -22,7 +22,7 @@ ENV_FILE_PATH="$PWD/src/.env"
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
   # Build the Docker image with user creation
   docker build -t $IMAGE_NAME -f- . <<EOF
-FROM debian:stable-slim
+FROM maniator/gh
 RUN apt-get update && apt-get install -y python3 python3-pip python3-venv git openssh-client
 RUN python3 -m venv /venv
 RUN useradd -m $USERNAME
@@ -33,7 +33,8 @@ fi
 docker run -it \
   -v $SSH_KEY_PATH:/home/$USERNAME/.ssh/id_ed25519_knarb \
   -v $SSH_PUB_KEY_PATH:/home/$USERNAME/.ssh/id_ed25519_knarb.pub \
-  -v $ENV_FILE_PATH:/home/$USERNAME/joe-sim-ai/src/.env \
+  -v $ENV_FILE_PATH:/home/$USERNAME/tmp/.env \
+  -v $HOME/.ssh/config_docker:/home/$USERNAME/.ssh/config \
   -w "/home/$USERNAME" \
   $IMAGE_NAME \
   /bin/bash -c "\
@@ -43,12 +44,20 @@ docker run -it \
     git config --global user.email 'knarbmakes@gmail.com' && \
     ssh-keyscan github.com >> /home/$USERNAME/.ssh/known_hosts && \
     chmod 600 /home/$USERNAME/.ssh/id_ed25519_knarb && \
+    chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/id_ed25519_knarb && \
+    chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/id_ed25519_knarb.pub && \
     eval \$(ssh-agent -s) && \
     ssh-add /home/$USERNAME/.ssh/id_ed25519_knarb && \
-    # Clone the repo
-    git clone git@github.com:knarbmakes/joe-sim-ai.git ./joe-sim-ai && \
-    cd ./joe-sim-ai && \
+    # Clone the repo if it doesn't exist, or pull if it does
+    if [ ! -d './joe-sim-ai' ]; then
+      git clone git@github.com:knarbmakes/joe-sim-ai.git ./joe-sim-ai
+    else
+      cd ./joe-sim-ai && git pull
+    fi && \
+    # Now move the .env file to the correct location
+    cp /home/$USERNAME/tmp/.env ./joe-sim-ai/src/.env && \
+    chown $USERNAME:$USERNAME ./joe-sim-ai/src/.env && \
     # Install dependencies
-    pip install -r src/requirements.txt && \
+    pip install -r joe-sim-ai/src/requirements.txt && \
     # Run your script
-    python $PYTHON_SCRIPT_PATH"
+    python joe-sim-ai/$PYTHON_SCRIPT_PATH"
