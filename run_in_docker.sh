f52c4829801abe5212210fe5e5befd6beefea309
Knarb <knarbmakes@gmail.com>
2023-12-03 16:06:29 -0500

    neo4j addition

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Set the path to your Python script relative to the source code directory
-PYTHON_SCRIPT_PATH="main.py"
-SOURCE_CODE_DIR="src"
+# Export the necessary environment variables
+export SSH_KEY_PATH="$HOME/.ssh/id_ed25519_knarb"
+export SSH_PUB_KEY_PATH="$HOME/.ssh/id_ed25519_knarb.pub"
+export ENV_FILE_PATH="$PWD/src/.env"
+export MEMORY_PATH="$PWD/memory"
+export DOWNLOADS_PATH="$PWD/downloads"
+export USERNAME="joe"
 
-# Docker image name
-IMAGE_NAME="joe-sim-sandbox"
-
-# Check if the Docker image already exists
-if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-  # Build the Docker image
-  docker build -t $IMAGE_NAME -f- . <<EOF
-FROM debian:stable-slim
-RUN apt-get update && apt-get install -y python3 python3-pip python3-venv
-RUN python3 -m venv /venv
-COPY $SOURCE_CODE_DIR /src
-RUN . /venv/bin/activate && pip install -r /src/requirements.txt
-EOF
-fi
-
-# Run the Docker container
-docker run -it -w "/src" $IMAGE_NAME /bin/bash -c ". /venv/bin/activate && python $PYTHON_SCRIPT_PATH"
+# Run Docker Compose
+docker-compose up --build
